c844d758b728057feefb11240b09cb2708dae8ac
somesh dwivedi <Pamli.Basak@dxglobal.com-at-376039757673>
2023-01-16 23:18:33 +0530

    added mongodb atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 mongoose.set('strictQuery', true);
-mongoose.connect("mongodb://localhost/ecommerse-db");
+mongoose.connect("mongodb+srv://somesh:1vM3kfBrY7SBSq82@cluster0.1ubo1xd.mongodb.net/test");
 
 const db=mongoose.connection;
 
@@ -9,3 +9,4 @@ db.on('error', console.error.bind(console, "err in connecting database"));
 db.once('open', ()=>{
     console.log("db is connected successfully")
 })
+
